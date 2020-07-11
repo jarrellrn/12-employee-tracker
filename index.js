0bdeac232351648b31952591a5554385fa27b26f
jarrellrn <jarrellrn@gmail.com>
2020-07-11 15:37:12 -0500

    started main function work

--- index.js
+++ index.js
@@ -10,26 +10,32 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 function main(){
-    inquirer
-  .prompt([
-    {
-      name: 'title',
-      message: 'What would you like to do?',
-      type: 'list',
-      choices: []
-    }
-  ])
-  .then(function(answers){
-    if (answers.license === "ISC"){
-        licenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
-    }
-  });
+    inquirer.prompt({
+        name: 'main',
+        message: 'What would you like to do?',
+        type: 'list',
+        choices: ["Add departments, roles, employees", "View departments, roles, employees", "Update employee roles"]
+    }).then(function(answers){
+        switch(answers.main){
+            case "Add departments, roles, employees":
+                console.log("You have chosen to add.");
+                break;
+            case "View departments, roles, employees":
+                console.log("You have chosen to view.");
+                break;
+            case "Update employee roles":
+                console.log("You have chosen to update.");
+                break;
+        }
+    });
 }
+
+main();
